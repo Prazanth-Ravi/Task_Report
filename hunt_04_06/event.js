6298aa591a93282b7d34217121477ad65aca2645
Prazanth-Ravi <prazanthofficial.dev@gmail.com>
2025-06-04 17:50:37 +0530

    code_hunt

---
+++ hunt_04_06/event.js
@@ -0,0 +1,56 @@
+// let a="Code challenges ignite minds"
+// let b=[]
+// for(let i=(a.length)-1, n=0; i>=0 ; i--, n++){
+//     b[n]=a[i]
+// }
+// console.log(b.join(""))
+
+// let a = "sdnim etingi segnellahc edoC"
+// let sum=0
+// for (let i = 0; i < a.length; i++) {
+//     if (a[i]=="a"||a[i]=="e"||a[i]=="i"||a[i]=="o"||a[i]=="u")
+//         sum=sum+1    
+// }
+//     console.log(sum);
+
+// let a=9
+// let b=a**2
+// console.log(b);
+
+// let a = 81
+// let b = "Algorithm"
+// let i = (a.length)
+// let N = a % i
+// console.log(N);
+
+//------------------- Answer for Riddle 1 is "ERROR" ---------------------------------
+
+// ______________________________________________________________________________________________
+
+// let a=29
+// let c=0
+// for(let i=0; i<= a; i++){
+//         (a%i==0) ? c++ : c
+//     if (c>2){
+//         console.log( a + " is not a Prime Num")
+//         break
+//     }
+// }
+// if(c==2){
+//     console.log(a + " is a Prime Number");
+// }
+
+// let a=2
+// let b=9
+// let sum=a+b
+// console.log(sum);
+
+// let a="Innovation"
+// let b=11
+// let N=(b%(a.length))
+
+
+// console.log(a[N]);
+
+// 
+// _____________________________________________________________________________________
